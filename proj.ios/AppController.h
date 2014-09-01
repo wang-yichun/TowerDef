3c3c48c8db2e0a53fd4e1a1318d1d4e885627d1d
wang-yichun <wang-yichun@meyougames.com>
2014-09-01 10:59:47 +0800

    proj.ios

---
+++ proj.ios/AppController.h
@@ -0,0 +1,13 @@
+
+
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+    
+}
+
+@end
+
